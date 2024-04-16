bae1d706ae900d6b2cccf994903515b276890d72
Lucas Berganton <lucas.berganton@mail.com>
2024-04-15 21:52:18 -0300

    Trocado o 'large' dos menus para uma macro 'MENU_WIDTH'

--- src/interface.c
+++ src/interface.c
@@ -27,23 +27,22 @@ void handleWithSmallTermSize(int y, int x) {
 }
 
 int interfaceMenu(const char *title, const int n, ...) {
-  static const int large = 30;
   int pos, input;
 
   do {
-    if (LINES < n + 4 || COLS < large) {
-      handleWithSmallTermSize(n + 4, large);
+    if (LINES < n + 4 || COLS < MENU_WIDTH) {
+      handleWithSmallTermSize(n + 4, MENU_WIDTH);
     }
 
     va_list args;
     va_start(args, n);
 
-    WINDOW* header = newwin(2, large, (LINES - (n + 4)) / 2, (COLS - large) / 2);
-    WINDOW* options = newwin(n + 2, large, (LINES - n) / 2, (COLS - large) / 2);
+    WINDOW* header = newwin(2, MENU_WIDTH, (LINES - (n + 4)) / 2, (COLS - MENU_WIDTH) / 2);
+    WINDOW* options = newwin(n + 2, MENU_WIDTH, (LINES - n) / 2, (COLS - MENU_WIDTH) / 2);
     wborder(header, ACS_VLINE, ACS_VLINE, ACS_HLINE, ' ', ACS_ULCORNER, ACS_URCORNER, ACS_VLINE, ACS_VLINE);
     wborder(options, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE, ACS_LTEE, ACS_RTEE, ACS_LLCORNER, ACS_LRCORNER);
 
-    wmove(header, 1, (large - strlen(title)) / 2);
+    wmove(header, 1, (MENU_WIDTH - strlen(title)) / 2);
     wprintw(header, "%s", title);
 
     for (register int i = 1; i <= n; i++) {
@@ -58,25 +57,25 @@ int interfaceMenu(const char *title, const int n, ...) {
     pos = 1;
 
     wmove(options, pos, 1);
-    wchgat(options, large - 2, A_REVERSE, 0, NULL);
+    wchgat(options, MENU_WIDTH - 2, A_REVERSE, 0, NULL);
 
     while ((input = wgetch(options)) != '\n' && input != KEY_RESIZE) {
       switch (input) {
       case 'w':
       case 'W':
       case KEY_UP:
-        wchgat(options, large - 2, A_NORMAL, 0, NULL);
+        wchgat(options, MENU_WIDTH - 2, A_NORMAL, 0, NULL);
         pos = (pos == 1) ? (int) n : pos - 1;
         wmove(options, pos, 1);
-        wchgat(options, large - 2, A_REVERSE, 0, NULL);
+        wchgat(options, MENU_WIDTH - 2, A_REVERSE, 0, NULL);
         break;
       case 's':
       case 'S':
       case KEY_DOWN:
-        wchgat(options, large - 2, A_NORMAL, 0, NULL);
+        wchgat(options, MENU_WIDTH - 2, A_NORMAL, 0, NULL);
         pos = (pos == (int) n) ? 1 : pos + 1;
         wmove(options, pos, 1);
-        wchgat(options, large - 2, A_REVERSE, 0, NULL);
+        wchgat(options, MENU_WIDTH - 2, A_REVERSE, 0, NULL);
       }
     }
 
@@ -94,23 +93,22 @@ int interfaceMenu(const char *title, const int n, ...) {
 }
 
 void interfaceString(const char *title, const int n, ...) {
-  static const int large = 30;
   int input;
 
   do {
-    if (LINES < n + 4 || COLS < large) {
-      handleWithSmallTermSize(n + 4, large);
+    if (LINES < n + 4 || COLS < MENU_WIDTH) {
+      handleWithSmallTermSize(n + 4, MENU_WIDTH);
     }
 
     va_list args;
     va_start(args, n);
 
-    WINDOW* header = newwin(2, large, (LINES - (n + 4)) / 2, (COLS - large) / 2);
-    WINDOW* options = newwin(n + 2, large, (LINES - n) / 2, (COLS - large) / 2);
+    WINDOW* header = newwin(2, MENU_WIDTH, (LINES - (n + 4)) / 2, (COLS - MENU_WIDTH) / 2);
+    WINDOW* options = newwin(n + 2, MENU_WIDTH, (LINES - n) / 2, (COLS - MENU_WIDTH) / 2);
     wborder(header, ACS_VLINE, ACS_VLINE, ACS_HLINE, ' ', ACS_ULCORNER, ACS_URCORNER, ACS_VLINE, ACS_VLINE);
     wborder(options, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE, ACS_LTEE, ACS_RTEE, ACS_LLCORNER, ACS_LRCORNER);
 
-    wmove(header, 1, (large - strlen(title)) / 2);
+    wmove(header, 1, (MENU_WIDTH - strlen(title)) / 2);
     wprintw(header, "%s", title);
 
     for (register int i = 1; i <= n; i++) {
